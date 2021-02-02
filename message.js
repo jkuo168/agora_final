8b79dac7b21be87fdbe3aff13c27bde206bf96f7
Joanna Kuo <jkuo@princeton.edu>
2021-02-02 01:48:04 -0500

    figured out messaging?

--- message.js
+++ message.js
@@ -3,6 +3,8 @@ let message_client = AgoraRTM.createInstance(appID, {
 });
 let channel = message_client.createChannel(channel_name);
 
+let message = message_client.createMessage({ text: "hello" });
+
 message_client
   .login({ uid: user_name })
   .then(() => {
@@ -10,14 +12,14 @@ message_client
       .join(appID)
       .then(() => {
         console.log("Successfully joined");
-        channel
-          .sendMessage({ text: "hello" })
-          .then(() => {
-            console.log("Successfully sent message");
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        // channel
+        //   .sendMessage(message)
+        //   .then(() => {
+        //     document.getElementById("chat").innerHTML = message.text;
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //   });
       })
       .catch((error) => {
         console.log(error);
@@ -28,3 +30,25 @@ message_client
   });
 
 console.log("here");
+
+let send = () => {
+  let message = message_client.createMessage({ text: "hello" });
+  channel
+    .sendMessage(message)
+    .then(() => {
+      let message_div = document.createElement("div");
+      message_div.innerHTML = message.text;
+      document.getElementById("chat").appendChild(message_div);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+channel.on("ChannelMessage", (message, memberId) => {
+  console.log("RECEIVED MESSAGE");
+  console.log(message, memberId);
+  let message_div = document.createElement("div");
+  message_div.innerHTML = message.text;
+  document.getElementById("chat").appendChild(message_div);
+});
